358d7525d5ce61e55ae9c6aebbfe5901a5572269
Nomosauce <sarah_atiqah05@hotmail.com>
2023-12-18 01:24:18 +0800

    shooting and me moving

--- Assets/SARAH ATIQAH BTE KAMARUDIN_Implementation/Assets/Scripts/Shooting.cs
+++ Assets/SARAH ATIQAH BTE KAMARUDIN_Implementation/Assets/Scripts/Shooting.cs
@@ -6,27 +6,29 @@ public class Shooting : MonoBehaviour
 {
 
     public GameObject missile;
-    //public Transform shootPt;
-    private Rigidbody rb;
-    private float mSpeed = 5f;
+    public Transform shootPt;
+    [SerializeField] private float mSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = -transform.forward * mSpeed;
+
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject mProjectile = Instantiate(missile, transform.position, transform.rotation);
+            GameObject missileObj = Instantiate(missile, shootPt.position, shootPt.rotation);
+            missileObj.GetComponent<Rigidbody>().velocity = shootPt.transform.up * mSpeed;
+
+            Destroy(missileObj, 5f);
         }
     }
 }
