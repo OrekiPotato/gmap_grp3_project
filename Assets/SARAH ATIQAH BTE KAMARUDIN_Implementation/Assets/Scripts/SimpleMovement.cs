032061562c53e3f0e589cbb86ecb34ef72c4c7fe
Nomosauce <sarah_atiqah05@hotmail.com>
2023-12-19 17:37:15 +0800

    comments

--- Assets/SARAH ATIQAH BTE KAMARUDIN_Implementation/Assets/Scripts/SimpleMovement.cs
+++ Assets/SARAH ATIQAH BTE KAMARUDIN_Implementation/Assets/Scripts/SimpleMovement.cs
@@ -5,13 +5,13 @@ using UnityEngine.EventSystems;
 
 public class SimpleMovement : MonoBehaviour
 {
-    private float horizontal;
+    private float horizontal; //input variables
     private float vertical;
-    private float mSpeed = 5f;
+    private float mSpeed = 5f; //player movement speed
 
     private Vector3 moveDir;
 
-    [SerializeField] private Rigidbody rb;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +22,13 @@ public class SimpleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal"); //captures the input value (-1, 0, 1) for both horizontal and vertical every frame so it can be used to update the player's direction (moveDir)
         vertical = Input.GetAxisRaw("Vertical");
         moveDir = new Vector3(horizontal, 0, vertical);
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = moveDir * mSpeed;
+        rb.velocity = moveDir * mSpeed; //apply linear velocity to the players rb with the speed to make the player move towards the direction that was inputted by the user
     }
 }
